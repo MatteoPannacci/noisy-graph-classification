0314d0c045daa4a02dfd7f980e7b7a4a391a46c0
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-25 17:02:35 +0200

    added symmetric cross entropy loss

--- src/losses.py
+++ src/losses.py
@@ -4,12 +4,28 @@ import torch.nn.functional
 
 
 class NoisyCrossEntropyLoss(torch.nn.Module):
+
     def __init__(self, p_noisy):
         super().__init__()
         self.p = p_noisy
         self.ce = torch.nn.CrossEntropyLoss(reduction='none')
 
+
     def forward(self, logits, targets):
         losses = self.ce(logits, targets)
         weights = (1 - self.p) + self.p * (1 - torch.nn.functional.one_hot(targets, num_classes=logits.size(1)).float().sum(dim=1))
         return (losses * weights).mean()
+
+
+
+class SymmetricCrossEntropyLoss(torch.nn.Module):
+    
+    def __init__(self):
+        self.ce = torch.nn.CrossEntropyLoss()
+        self.rce = torch.nn.CrossEntropyLoss()
+
+
+    def forward(self, logits, targets):
+        ce_loss = self.ce(logits, targets)
+        rce_loss = self.rce(targets, logits)
+        return ce + rce
