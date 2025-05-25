367c69f479d104a481a639cf2423724ac681ba2f
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-25 18:10:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -147,6 +147,41 @@ def plot_progress(split_name, losses, accuracies, f1_scores, output_dir):
     plt.close()
 
 
+def plot_all(train_losses, train_accuracies, train_f1s, val_losses, val_accuracies, val_f1s, output_dir):
+    epochs = range(1, len(losses) + 1)
+    plt.figure(figsize=(18, 6))
+
+    # Plot losses
+    plt.subplot(1, 3, 1)
+    plt.plot(epochs, train_losses, label=f"Train Loss", color='red')
+    plt.plot(epochs, val_losses, label=f"Val Loss", color='blue')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(f'Loss per Epoch')
+
+    # Plot accuracies
+    plt.subplot(1, 3, 2)
+    plt.plot(epochs, train_accuracies, label=f"Train Accuracy", color='red')
+    plt.plot(epochs, val_accuracies, label=f"Val Accuracy", color='blue')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title(f'Accuracy per Epoch')
+
+    # Plot f1 scores
+    plt.subplot(1, 3, 3)
+    plt.plot(epochs, train_f1s, label=f"Train F1 Score", color='red')
+    plt.plot(epochs, val_f1s, label=f"Val F1 Score", color='blue')
+    plt.xlabel('Epoch')
+    plt.ylabel('F1 Score')
+    plt.title(f'F1 Score per Epoch')
+
+    # Save plots in the current directory
+    os.makedirs(output_dir, exist_ok=True)
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, f"full_plot.png"))
+    plt.close()
+
+
 def main(args):
 
     # Set the random seed
@@ -292,9 +327,11 @@ def main(args):
                 logger.info(f"Best model updated and saved at {checkpoint_path}")
 
         # Plot training progress in current directory
-        plot_progress("Training", train_losses, train_accuracies, train_f1s, os.path.join(logs_folder, "plots"))
+        plot_progress("Training", train_losses, train_accuracies, train_f1s, os.path.join(logs_folder, "plotsTrain"))
         if use_validation:
             plot_progress("Validation", val_losses, val_accuracies, val_f1s, os.path.join(logs_folder, "plotsVal"))
+            plot_all(train_losses, train_accuracies, train_f1s, val_losses, val_accuracies, val_f1s, os.path.join(logs_folder, "plotsAll"))
+
 
         # DELETE TRAIN DATASET VARIABLES
         if use_validation:
