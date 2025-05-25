2f6f86566cd9523c6dc342c8cdda97cb874da422
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-25 17:45:42 +0200

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -30,8 +30,8 @@ class SymmetricCrossEntropyLoss(torch.nn.Module):
 
         ce_loss = self.ce(logits, targets)
 
-        one_hot_targets = F.one_hot(targets, num_classes=self.num_classes).float()
-        pred_probs = F.softmax(logits, dim=1).clamp(min=1e-7, max=1.0)
+        one_hot_targets = torch.nn.functional.one_hot(targets, num_classes=self.num_classes).float()
+        pred_probs = torch.nn.functional.softmax(logits, dim=1).clamp(min=1e-7, max=1.0)
         rce_loss = -torch.sum(pred_probs * torch.log(one_hot_targets.clamp(min=1e-7)), dim=1).mean()
 
         return ce_loss + rce_loss
