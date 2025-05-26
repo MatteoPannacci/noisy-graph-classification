edeac540495cff84b6e35f1e0ab28711de8ed5af
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-26 11:01:41 +0200

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -5,15 +5,17 @@ import torch.nn.functional
 
 class NoisyCrossEntropyLoss(torch.nn.Module):
 
-    def __init__(self, p_noisy, weight=None):
+    def __init__(self, p_noisy):
         super().__init__()
         self.p = p_noisy
-        self.ce = torch.nn.CrossEntropyLoss(reduction='none', weight=weight)
+        self.ce = torch.nn.CrossEntropyLoss(reduction='none')
 
 
     def forward(self, logits, targets):
         losses = self.ce(logits, targets)
         weights = (1 - self.p) + self.p * (1 - torch.nn.functional.one_hot(targets, num_classes=logits.size(1)).float().sum(dim=1))
+        print(weights)
+        assert False
         return (losses * weights).mean()
 
 
