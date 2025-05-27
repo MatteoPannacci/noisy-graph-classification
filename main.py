3b2b62ca77f95a4248439e959a5004045be6d933
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-27 18:28:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,8 @@ def evaluate(data_loader, model, device, calculate_accuracy=False, return_labels
         return  total_loss / len(data_loader), accuracy, f1_score
     
     if return_labels:
+        print(f"True Labels: {true_labels}")
+        print(f"True Labels Numpy: {true_labels.cpu().numpy()}")
         return pred_labels.cpu().numpy(), true_labels.cpu().numpy()
 
     else:
