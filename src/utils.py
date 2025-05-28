b86133d2cc87b1528f971e366671c44cd26dbef5
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-28 11:56:30 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,11 @@ import tarfile
 import os
 from src.loadData import GraphDataset
 from torch_geometric.loader import DataLoader
+import csv
+import pandas as pd
+import random
+import sys
+from collections import defaultdict, Counter
 
 
 def add_zeros(data):
@@ -46,3 +51,47 @@ def compute_label_distribution(loader):
             counters[label] += 1
 
     return counters
+
+
+def save_predictions(predictions, test_path):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    submission_folder = os.path.join(script_dir, "submission")
+    test_dir_name = os.path.basename(os.path.dirname(test_path))
+
+    os.makedirs(submission_folder, exist_ok=True)
+    output_csv_path = os.path.join(submission_folder, f"testset_{test_dir_name}.csv")
+
+    test_graph_ids = list(range(len(predictions)))
+    output_df = pd.DataFrame({
+        "id": test_graph_ids,
+        "pred": predictions
+    })
+
+    output_df.to_csv(output_csv_path, index=False)
+    print(f"Predictions saved to {output_csv_path}")
+
+
+
+def majority_vote(csv_files, output_file='majority_vote.csv'):
+    pred_counts = defaultdict(list)
+
+    # Read predictions from all CSV files
+    for file in csv_files:
+        df = pd.read_csv(file)
+        for _, row in df.iterrows():
+            pred_counts[row['id']].append(row['pred'])
+
+    # Compute majority vote
+    final_preds = []
+    for id_, preds in pred_counts.items():
+        counter = Counter(preds)
+        most_common = counter.most_common()
+        max_count = most_common[0][1]
+        top_preds = [pred for pred, count in most_common if count == max_count]
+        final_pred = random.choice(top_preds)  # break ties randomly
+        final_preds.append({'id': id_, 'pred': final_pred})
+
+    # Save to output CSV
+    output_df = pd.DataFrame(final_preds)
+    output_df.to_csv(output_file, index=False)
+    print(f"Saved majority vote results to {output_file}")
